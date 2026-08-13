b040641a4a5a9c31c557e1607915f5c667048497
Shree <khape.jai@gmail.com>
2026-08-13 18:51:57 +0530

    Adv Version

--- app.py
+++ app.py
@@ -4,6 +4,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "🏠 Home Page"
+
+@app.route("/about")
+def about():
+    return "ℹ️ About Page"
+
+@app.route("/contact")
+def contact():
+    return "📞 Contact Page"
+
+@app.route("/services")
+def services():
+    return "Services"
+
+@app.route("/profile")
+def profile():
+    return "Welcome to my profile!"
 
 app.run(debug=True)
