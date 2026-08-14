9578a9a6bcad6dae53350030a2dfa1cd09f03014
Shree <khape.jai@gmail.com>
2026-08-14 19:21:39 +0530

    1st HTML + Flask

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "🏠 Home Page"
+    return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return "ℹ️ About Page"
-
-@app.route("/contact")
-def contact():
-    return "📞 Contact Page"
-
-@app.route("/services")
-def services():
-    return "Services"
-
-@app.route("/profile")
-def profile():
-    return "Welcome to my profile!"
 
 app.run(debug=True)
