9b07d4aa8a44677414c98d9dadff3cea26e21df1
Shree <khape.jai@gmail.com>
2026-08-14 20:05:50 +0530

    Add Jinja2 conditional rendering with if and else

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    is_student = False
 
-@app.route("/welcome", methods=["POST"])
-def welcome():
-    name = request.form["username"]
-    return f"Welcome {name}"
+    return render_template("index.html", is_student=is_student)
+
 
 app.run(debug=True)
