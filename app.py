e62a9bdda51a46badc007dffa9d974874208eb66
Shree <khape.jai@gmail.com>
2026-08-15 18:55:02 +0530

    feat: implement delete functionality for books

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    
+
     conn = sqlite3.connect("books.db")
     cursor = conn.cursor()
 
-    cursor.execute(
-        "SELECT * FROM books")
+    cursor.execute("SELECT * FROM books")
 
     books = cursor.fetchall()
 
     conn.close()
 
     return render_template("index.html", books=books)
+
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -28,13 +28,13 @@ def add():
     conn = sqlite3.connect("books.db")
     cursor = conn.cursor()
 
-    cursor.execute(
-        "INSERT INTO books(name, author) VALUES(?, ?)", (name, author))
-    
+    cursor.execute("INSERT INTO books(name, author) VALUES(?, ?)", (name, author))
+
     conn.commit()
     conn.close()
 
     return redirect("/")
+
 
 @app.route("/update", methods=["POST"])
 def update_book():
@@ -47,12 +47,29 @@ def update_book():
     cursor = conn.cursor()
 
     cursor.execute(
-        "UPDATE books SET name=?, author=? WHERE id=?",
-        (name, author, book_id))
-   
+        "UPDATE books SET name=?, author=? WHERE id=?", (name, author, book_id)
+    )
+
     conn.commit()
     conn.close()
 
     return redirect("/")
+
+
+@app.route("/delete", methods=["POST"])
+def delete_book():
+
+    book_id = request.form["id"]
+
+    conn = sqlite3.connect("books.db")
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM books WHERE id=?", (book_id,))
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/")
+
 
 app.run(debug=True)
