8b3891da0d861cfe1582b1dbcfd69abb74bf6e49
Shree <khape.jai@gmail.com>
2026-08-16 23:15:23 +0530

    feat: implement secure user login with password verification

--- Authentication/app.py
+++ Authentication/app.py
@@ -1,10 +1,10 @@
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, request, render_template
 import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/register")
 def register():
 
     return render_template("register.html")
@@ -26,6 +26,32 @@ def register_user():
     conn.close()
     
     return "Registration Successfully!"
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_user():
+
+    username = request.form["username"]
+    password = request.form["password"]
+
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT password FROM users WHERE username=?",
+        (username,))
+
+    user = cursor.fetchone()
+
+    conn.close()
+
+    if user and check_password_hash(user[0], password):
+        return "Login Successful!"
+    else:
+        return "Invalid Username or password!" 
 
 if __name__ == "__main__":
     app.run(debug=True)    
