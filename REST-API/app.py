350d8bb5366a1e35b62e6d2e201db5c32a912de3
Shree <khape.jai@gmail.com>
2026-08-18 21:59:31 +0530

    feat: add POST endpoint for student API

--- REST-API/app.py
+++ REST-API/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -15,6 +15,16 @@ def home():
 @app.route("/students")
 def get_students():
     return jsonify(students)
+
+@app.route("/students", methods=["POST"])
+def add_student():
+
+    data = request.get_json()
+    students.append(data)
+
+    return jsonify({
+        "message": "Student Added Successfully!",
+        "students": students}),201    
 
 if __name__ == "__main__":
     app.run(debug=True)
