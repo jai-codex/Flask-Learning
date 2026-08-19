d13fa1902e435bbab5fc996a83dc270b642db21e
Shree <khape.jai@gmail.com>
2026-08-19 08:20:23 +0530

    feat: add PUT endpoint to update student data

--- REST-API/app.py
+++ REST-API/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 students = [{
     "id" : 1,
     "name" : "Jai",
-    "ahe" : 19
+    "age" : 19
 }]
 
 @app.route("/")
@@ -25,6 +25,20 @@ def add_student():
     return jsonify({
         "message": "Student Added Successfully!",
         "students": students}),201    
+
+@app.route("/students/<int:id>", methods=["PUT"])
+def update_name(id):
+
+    data = request.get_json()
+
+    for student in students:
+        if student["id"] == id:
+            student["name"] = data["name"]
+            student["age"] = data["age"]
+
+            return jsonify({"message": "Student Updated",
+            "student": student}),200
+    return jsonify({"message": "Student Not Found"}),404        
 
 if __name__ == "__main__":
     app.run(debug=True)
