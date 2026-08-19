578d6c10e63b031990c182fe0b6365102c84d85c
Shree <khape.jai@gmail.com>
2026-08-19 22:03:53 +0530

    feat: connect Flask GET endpoint to SQLite database

--- REST-API/app.py
+++ REST-API/app.py
@@ -1,54 +1,20 @@
 from flask import Flask, jsonify, request
+import sqlite3
 
-app = Flask(__name__)
-
-students = [{
-    "id" : 1,
-    "name" : "Jai",
-    "age" : 19
-}]
-
-@app.route("/")
-def home():
-    return "Students API is Running!"
+app = Flask("__name__")
 
 @app.route("/students")
 def get_students():
+
+    conn = sqlite3.connect("students.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM students")
+
+    students = cursor.fetchall()
+    conn.close()
+    
     return jsonify(students)
 
-@app.route("/students", methods=["POST"])
-def add_student():
-
-    data = request.get_json()
-    students.append(data)
-
-    return jsonify({
-        "message": "Student Added Successfully!",
-        "students": students}),201    
-
-@app.route("/students/<int:id>", methods=["PUT"])
-def update_name(id):
-
-    data = request.get_json()
-
-    for student in students:
-        if student["id"] == id:
-            student["name"] = data["name"]
-            student["age"] = data["age"]
-
-            return jsonify({"message": "Student Updated",
-            "student": student}),200
-    return jsonify({"message": "Student Not Found"}),404        
-
-@app.route("/students/<int:id>", methods=["DELETE"])
-def delete_student(id):
-
-    for student in students:
-        if student["id"] == id:
-            students.remove(student)
-            return jsonify({"message": "Student Deleted Successfully!"}),200
-    return jsonify({
-        "message": "Student Not Found"}),404
-        
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(debug=True)
