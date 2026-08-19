f35ff4aefc05da4691a2d0b1d7a1028e56a0c619
Shree <khape.jai@gmail.com>
2026-08-19 22:37:12 +0530

    feat: implement DELETE API with SQLite

--- REST-API/app.py
+++ REST-API/app.py
@@ -3,19 +3,20 @@ import sqlite3
 
 app = Flask("__name__")
 
+
 @app.route("/students")
 def get_students():
 
     conn = sqlite3.connect("students.db")
     cursor = conn.cursor()
 
-    cursor.execute(
-        "SELECT * FROM students")
+    cursor.execute("SELECT * FROM students")
 
     students = cursor.fetchall()
     conn.close()
-    
+
     return jsonify(students)
+
 
 @app.route("/students", methods=["POST"])
 def add_student():
@@ -26,14 +27,14 @@ def add_student():
     cursor = conn.cursor()
 
     cursor.execute(
-        "INSERT INTO students(name, age) VALUES(?, ?)",
-        (data["name"], data["age"]))
+        "INSERT INTO students(name, age) VALUES(?, ?)", (data["name"], data["age"])
+    )
 
     conn.commit()
     conn.close()
 
-    return jsonify({
-        "message": "Student Added Sucessfully!"}), 201    
+    return jsonify({"message": "Student Added Sucessfully!"}), 201
+
 
 @app.route("/students/<int:id>", methods=["PUT"])
 def update_student(id):
@@ -44,14 +45,27 @@ def update_student(id):
     cursor = conn.cursor()
 
     cursor.execute(
-        "UPDATE students SET name=?, age=? WHERE id=?",
-        (data["name"],data["age"], id))
+        "UPDATE students SET name=?, age=? WHERE id=?", (data["name"], data["age"], id)
+    )
 
     conn.commit()
     conn.close()
 
-    return jsonify({
-        "message": "Student Updated Successfully!"
-    }), 200 
+    return jsonify({"message": "Student Updated Successfully!"}), 200
+
+
+@app.route("/students/<int:id>", methods=["DELETE"])
+def delete_student(id):
+
+    conn = sqlite3.connect("students.db")
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM students WHERE id=?", (id,))
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Student Deleted Successfully!"}), 200
+
 
 app.run(debug=True)
