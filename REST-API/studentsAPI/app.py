1030409db941296e568bed69bb768d593d43e31d
Shree <khape.jai@gmail.com>
2026-08-21 18:45:42 +0530

    feat: configure JWT authentication in Flask

--- REST-API/studentsAPI/app.py
+++ REST-API/studentsAPI/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from routes import register_routes
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = "mysecretkey"
+
+jwt = JWTManager(app)
 
 register_routes(app)
 
